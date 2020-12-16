4cb78006c97c1af22e6b85ac410655265c4d63fd
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-16 18:28:37 -0300

    deploy update

--- email/IEmail.cs
+++ email/IEmail.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace trifenix.connect.interfaces.email
+namespace trifenix.connect.interfaces
 {
     /// <summary>
     /// interface encargada de envíar emails
--- graph/IGraphApi.cs
+++ graph/IGraphApi.cs
@@ -2,6 +2,7 @@
 
 namespace trifenix.connect.interfaces.graph
 {
+    
     public interface IGraphApi {
 
         Task<string> CreateUserIntoActiveDirectory(string name, string email);
