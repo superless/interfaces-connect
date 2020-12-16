b55db618c270f80dfa0a85d98561c9c2ef38dac3
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-16 18:41:45 -0300

    ultima actualización antes de subir

--- graph/IGraphApi.cs
+++ graph/IGraphApi.cs
@@ -1,10 +1,20 @@
 ﻿using System.Threading.Tasks;
 
+
 namespace trifenix.connect.interfaces.graph
 {
-    
+
+    /// <summary>
+    /// Manejo de usuarios en active directory
+    /// </summary>
     public interface IGraphApi {
 
+        /// <summary>
+        /// Crea un usuario en active directory
+        /// </summary>
+        /// <param name="name">nombre del usuario</param>
+        /// <param name="email">correo</param>
+        /// <returns>token</returns>
         Task<string> CreateUserIntoActiveDirectory(string name, string email);
 
     }
--- hash/IHashSearchHelper.cs
+++ hash/IHashSearchHelper.cs
@@ -10,18 +10,19 @@ namespace trifenix.connect.interfaces.hash
     /// Esta interface obliga a asignar un hash para el header, es decir lograr un hash desde el diccionario
     /// e un entitySearch, es decir cuales son los nombres de los indices.
     /// Esto permitirá saber si los índices han cambiado.
-    /// 
     /// También existe un hash generado por el json de un elemento de base de datos de persistencia,
     /// esto permitirá validar que el entitySearch sea un elemento de la base de datos.
     /// </summary>
     public interface IHashSearchHelper
     {
         /// <summary>
-        /// Convierte un objeto en un hash.
+        /// Convierte un objeto en un hash
+        /// esto se usa para asignar un hash a un entitySearch.
+        /// De esta manera si el resultado de un hash de un elemento entitySearch
+        /// es distinto al que se espera, el objeto habrá sido corrompido.
         /// </summary>
-        /// <typeparam name="T2">tipo de dato de base de datos de persistencia</typeparam>
-        /// <param name="model">elemento a convertir en hash</param>
-        /// <returns>Hash del elemento</returns>
+        /// <param name="obj">modelo a hashear</param>
+        /// <returns>hash</returns>
         string HashModel(object obj);
 
         /// <summary>
@@ -30,7 +31,6 @@ namespace trifenix.connect.interfaces.hash
         /// con esto todas las colecciones tendrán un único hash.
         /// Si llegase a modificar los índices de las cabeceras, el hash sería diferente al resto.
         /// </summary>
-        /// <typeparam name="T2">Tipo de dato para base de datos persistente</typeparam>
         /// <returns>Hash de cabeceras</returns>
         string HashHeader(Type type);
     }
