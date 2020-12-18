2a010421a114bac675a19df1a45e15a96bee009c
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-17 21:53:05 -0300

    ahora si hay log

---
+++ log/Namespace.cs
@@ -0,0 +1,12 @@
+
+namespace trifenix.connect.interfaces.log
+{
+    /// <summary>
+    /// ## trifenix.connect.interfaces.hash
+    /// Namespace donde se encontrarán las interfaces logs
+    /// necesarias para dejar el registro de las actividades internas.
+    /// 
+    /// ///</summary>
+    internal class Namespace
+    {}
+}
