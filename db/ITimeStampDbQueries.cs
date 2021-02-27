32d710e7f657e442df4d0f96bbd83c1537c23290
yo-fenix <alejandro.iglesias@trifenix.io>
2021-02-27 20:21:09 -0300

    Se agregan intefaces de db.

---
+++ db/ITimeStampDbQueries.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using trifenix.model;
+
+namespace trifenix.connect.interfaces.db
+{
+    public interface ITimeStampDbQueries {
+        Task<long[]> GetTimestamps<T>() where T: DocumentDb;
+    }
+}
